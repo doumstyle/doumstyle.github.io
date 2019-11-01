2a4c479ed0af4b6fde6cb9e34f8dd019efc509c1
Mamadou AMADOU <doumstyle@gmail.com>
2019-11-01 11:24:58 +0100

    deploy

---
+++ precache-manifest.c43818a496867176ad6ec6ca6c68807e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c0b33741b878117e33",
+    "url": "/css/app.05b0fd4b.css"
+  },
+  {
+    "revision": "c1163216af62a6d4337f834c4a464109",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39c0b33741b878117e33",
+    "url": "/js/app.e76f62fc.js"
+  },
+  {
+    "revision": "f96c87c012d27b46aa2a",
+    "url": "/js/chunk-vendors.7724d0e0.js"
+  },
+  {
+    "revision": "017b5d3dd8ae66da3519f8cae5b06931",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
